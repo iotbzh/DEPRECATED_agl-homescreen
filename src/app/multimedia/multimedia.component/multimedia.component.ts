f4d0aeedb4ce4966a5b49aa180a8bcdf3e0c5cc4
Sebastien Douheret <sebastien.douheret@iot.bzh>
2018-03-20 22:39:53 +0100

    Fixed Multimedia load page when running on target.
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- src/app/multimedia/multimedia.component/multimedia.component.ts
+++ src/app/multimedia/multimedia.component/multimedia.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from "../../../environments/environment";
+
 @Component({
-  selector: 'multimedia',
-  templateUrl: 'multimedia.component.html',
-  styleUrls: ['multimedia.component.css']
+    selector: 'multimedia',
+    templateUrl: 'multimedia.component.html',
+    styleUrls: ['multimedia.component.css']
 })
 export class MultimediaComponent implements OnInit {
-  url = 'http://localhost:8000/assets/webgl2examples/dof.html'
-  constructor() {}
-  ngOnInit() {}
+    public url: string;
+
+    constructor() {
+        let env = environment.service || {
+            ip: 'localhost',
+            port: null,
+            api_url: '/api'
+        };
+        this.url = 'http://' + env.ip;
+        if (env.port) {
+            this.url += ':' + env.port;
+        }
+        this.url += '/assets/webgl2examples/dof.html';
+    }
+
+    ngOnInit() { }
 }
